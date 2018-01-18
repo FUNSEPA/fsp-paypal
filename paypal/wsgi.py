0da783b227a9e2d832d91626230947bd359f9f69
Luis <chuik@donarfunciona.com>
2018-01-18 17:51:36 +0100

    Migración

--- paypal/wsgi.py
+++ paypal/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "paypal.local_settings")
+os.environ["DJANGO_SETTINGS_MODULE"] = "paypal.local_settings"
 
 application = get_wsgi_application()
